14fab86c0feed081491ba22589fc20ca2e302405
AlenSkaria <hello.skaria@gmail.com>
2025-03-14 19:59:25 +0530

    -

--- frontend/ERPClient/src/pages/inventory/Inventory.tsx
+++ frontend/ERPClient/src/pages/inventory/Inventory.tsx
@@ -33,7 +33,7 @@ const Inventory = () => {
     return <div className="flex items-center justify-center h-screen text-red-500">{error}</div>;
 
   return (
-    <div className="flex justify-center min-h-screen bg-gray-50">
+    <div className="flex justify-center min-h-screen ">
       <div className="w-full max-w-7xl px-4">
       <h1 className="text-2xl font-bold mb-6">Inventory</h1>
         <div className="bg-white rounded-lg shadow-sm overflow-hidden">
